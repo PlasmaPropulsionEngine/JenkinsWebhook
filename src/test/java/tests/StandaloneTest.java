f8207d473cebce7c53b6738d36ffad2acfbc8db2
karan <karansalve13@gmail.com>
2024-08-30 16:59:36 +0530

    third commit

--- src/test/java/tests/StandaloneTest.java
+++ src/test/java/tests/StandaloneTest.java
@@ -26,7 +26,7 @@ import test_Components.BaseTest;
 
 public class StandaloneTest extends BaseTest 
 {
-	 //zara coat 3 is Product Name
+	 //zara coat 3 is Product Name items
 	String productName="ZARA COAT 3";
 	
 @Test(retryAnalyzer = test_Components.Retry.class)	
